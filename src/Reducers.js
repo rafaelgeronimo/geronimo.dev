195306acacc1caaaca7cac97ba86b72ac0d16c5a
Rafael Geronimo <rafaelgeronimo@outlook.com>
2019-11-18 13:46:00 -0300

    adjust

---
+++ src/Reducers.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+import userReducer from './reducers/userReducer';
+const Reducers = combineReducers({
+    user:userReducer
+});
+
+export default Reducers;
