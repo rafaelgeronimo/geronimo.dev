721b7d16b877eebb8703c44d138c86ba90bda91f
Rafael Gerônimo <rafaelg@VEGA.INTRA>
2020-01-03 04:28:14 -0300

    Remove old files

--- src/Reducers.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import userReducer from './reducers/userReducer';
-const Reducers = combineReducers({
-    user:userReducer
-});
-
-export default Reducers;
